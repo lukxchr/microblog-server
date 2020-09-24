7bbf7269c457ef699fa701f7c983890dec3e5e79
Lukasz Chrzanowski <lukaszchrzanowski@me.com>
2020-09-24 12:51:00 +0200

    cors

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -86,7 +86,7 @@ export class UserResolver {
     try {
       await em.persistAndFlush(user);
     } catch (err) {
-      if (err.code === "23505" || err.detail.includes("already exists")) {
+      if (err.code === "23505" || err.detail?.includes("already exists")) {
         return {
           errors: [
             {
