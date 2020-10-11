53bb97b037529559e16cc9816e03b2fe6a9316ec
Lukasz Chrzanowski <lukxchr@gmail.com>
2020-10-11 10:42:41 +0200

    fetch user(creator) data in posts query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -40,16 +40,31 @@ export class PostResolver {
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(limit, 100);
     const realLimitPlusOne = realLimit + 1;
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("posts")
-      .orderBy('"createdAt"', "DESC")
-      .take(realLimitPlusOne);
+
+    const replacements: any[] = [realLimitPlusOne];
     if (cursor) {
-      qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) }); //if cursor == p1.createdAt => get posts older than p1 (p1 not included)
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    const posts = await qb.getMany();
+    const posts = await getConnection().query(
+      `
+    select p.*,
+    json_build_object(
+      'id', u.id,
+      'username', u.username,
+      'email', u.email,
+      'createdAt', u."createdAt",
+      'updatedAt', u."updatedAt"
+      ) creator
+    from post p
+    inner join public.user u on u.id = p."creatorId"
+    ${cursor ? `where p."createdAt" < $2` : ""}
+    order by p."createdAt" DESC
+    limit $1
+    `,
+      replacements
+    );
+
     return {
       posts: posts.slice(0, realLimit),
       hasMore: posts.length === realLimitPlusOne,
